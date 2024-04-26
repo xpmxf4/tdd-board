cf71ccd7fae50e14676a0d3bc8b5d327c4642049
dev-chpark <xpmxf5@gmail.com>
2024-04-26 16:06:28 +0900

    🎉 init: 풀더 구조 세팅 완료

--- src/main/java/com/study/tddboard/TddBoardApplication.java
+++ src/main/java/com/study/tddboard/TddBoardApplication.java
@@ -2,7 +2,9 @@ package com.study.tddboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.*;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class TddBoardApplication {
 
