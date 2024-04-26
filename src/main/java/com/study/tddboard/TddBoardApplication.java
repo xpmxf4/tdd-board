1807f93ddd57de02c325df6194d80175e1b8859f
dev-chpark <xpmxf5@gmail.com>
2024-04-26 15:23:30 +0900

    🎉 init: Initial Commit

---
+++ src/main/java/com/study/tddboard/TddBoardApplication.java
@@ -0,0 +1,13 @@
+package com.study.tddboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TddBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/study/tddboard/TddBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.tddboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
